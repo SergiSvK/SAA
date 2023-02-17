8b822c3c920a0073de90d99d90ff608c48de4dae
sergio <s.k.valero@gmail.com>
2023-02-17 12:36:15 +0100

    Regresión Lineal 📈

---
+++ Regresión Logistica/logistica_binaria-DESKTOP-RLRV8RD/binaria.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import math
+# drive mount
+from google.colab import drive
+import math
+
+drive.mount('/content/drive')
+
+df = pd.read_csv('/content/drive/MyDrive/Datasets/insurance_data.csv')
+
+# mostrar columnas
+print(df.columns)
+
+plt.scatter(df['age'], df['bought_insurance'], marker='+', color='red')
+
+X = df['age']
+
+y = df['bought_insurance']
+
+# train test split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+
+# clarification binaria
+model = LogisticRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+model.predict_proba(X_test)
+
+model.score(X_test, y_test)
+
+# intercep
+model.intercept_
+
+# coeficiente
+model.coef_
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+# 0.5 = 1 / (1 + e^(-x))
+
+
+def prediction_function(data, coef, intercept):
+    z = coef * data + intercept
+    y = sigmoid(z)
+    return y
+
+
+age: int = 25
+
+prediction_function(age, model.coef_[0][0], model.intercept_[0])
